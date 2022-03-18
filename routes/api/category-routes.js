378f1c9062360af8ada1fed9f7f634cda4d7c534
Maryia <malakhava@yahoo.com>
2022-03-18 02:15:19 -0400

    corrections

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,10 +52,6 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.create({
     category_name: req.body.category_name
 })
@@ -66,8 +62,8 @@ router.put('/:id', (req, res) => {
 });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
   Category.update(req.body, {
     where: {
         id: req.params.id
@@ -84,6 +80,28 @@ router.delete('/:id', (req, res) => {
     console.log(err);
     res.status(500).json(err);
 });
+  
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+    }
+})
+.then(dbCatData => {
+    if (!dbCatData) {
+        res.status(404).json({ message: 'No category found with given id.' });
+        return;
+    }
+    res.json(dbCatData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+  
 });
 
 module.exports = router;
