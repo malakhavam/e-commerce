a7b1c1cedd6414793b0bc5f2fbd8a59273fcf01c
Maryia <malakhava@yahoo.com>
2022-03-19 04:39:26 -0400

    fixed port issues

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+
+// All data from .env is loaded into process.env
+require('dotenv').config(); 
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 }
-else {
-  sequelize=  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3001,
-      
-    });
-  } 
+
 
 module.exports = sequelize;
